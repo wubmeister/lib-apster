3b598324167849b777b13dcd6b8dfded34c650fd
Wubbo Bos <wubbo@addnoise.nl>
2018-04-09 09:37:11 +0200

    Get adapter config from config file

--- tests/Db/Adapter/Pdo/MysqlTest.php
+++ tests/Db/Adapter/Pdo/MysqlTest.php
@@ -9,11 +9,18 @@ class MysqlTest extends Testcase
 {
     public function testCreate()
     {
+        $config = include "../../../config.php";
+        $adapterConfig = $confg['db'];
+        unset($adapterConfig['adapter']);
+        adapter = new Mysql($adapterConfig);
+
+        /*
         $adapter = new Mysql([
             'host' => '127.0.0.1',
             'username' => 'root',
             'password' => 'wubje318',
             'dbname' => 'apster_test'
         ]);
+        */
     }
 }

e3cb6a1e210f81d1f91fcf06563d556b16c7facc
Wubbo Bos <wubbo@addnoise.nl>
2018-04-09 09:27:52 +0200

    Add test config file. Copy config.exmaple.php to config.php and edit that one

---
+++ tests/config.example.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'db' => [
+        'adapter' => 'Mysql',
+        'host' => '127.0.0.1',
+        'username' => 'DB_USERNAME',
+        'password' => 'DB_PASSWORD',
+        'dbname' => 'apster_test'
+    ]
+];
